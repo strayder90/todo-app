f521053d2bd08c3e801f14a120d1a41e0b8d6daf
Strayder90 <strayder@live.com>
2023-09-02 16:24:01 +0200

    Set up for simple todo project...

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
 root.render(
-<React.StrictMode>
-  <App />
-</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
