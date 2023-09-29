4e4bfa413a8dc8ad7649001eaf5dbd054fb88b4e
Strayder90 <strayder@live.com>
2023-09-30 00:35:55 +0200

    Added react route package. Established new route for deleted content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Deleted } from "@components/Deleted";
 
 import { Todo } from "@components/Todo.jsx";
 
 function App() {
   return (
     <>
-      <Todo text={"Todo List"} />
+      <BrowserRouter>
+        <Todo text={"Todo List"} />
+        <Routes>
+          <Route exact path="/deleted" element={<Deleted />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Deleted.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Deleted = () => {
+  return (
+    <>
+      <h2>Deleted</h2>
+    </>
+  );
+};
