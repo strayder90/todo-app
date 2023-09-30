d474eb1e2bd990ce3d61924b4ca57ce89849b77c
Strayder90 <strayder@live.com>
2023-09-30 23:25:48 +0200

    Need to handle delete.

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// Not needed to use useNavigate because there are no multiple pages here. But usefull to improve app.
 
 export const AddTodo = () => {
   const initialTodo = "";
   const [todo, setTodo] = useState(initialTodo);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const newTodo = { todo };
 
-    console.log(newTodo);
+    setIsLoading(true);
+
+    fetch("http://localhost:8000/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newTodo),
+    }).then(() => {
+      console.log("New todo added.");
+      setIsLoading(false);
+      navigate("/");
+    });
   };
 
   return (
@@ -18,9 +33,16 @@ export const AddTodo = () => {
         <div className="field">
           <input value={todo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="Add todo" required />
         </div>
-        <button className="ui button button-add" type="submit">
-          Save
-        </button>
+        {!isLoading && (
+          <button className="ui button button-add" type="submit">
+            Save
+          </button>
+        )}
+        {isLoading && (
+          <button disabled className="ui button button-add" type="submit">
+            Saving todo...
+          </button>
+        )}
       </form>
     </>
   );
