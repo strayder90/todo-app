267908f75dbf0a829738829a192791c5475a92c3
Strayder90 <strayder@live.com>
2023-10-01 15:18:30 +0200

    Installed webpack packages, babel packages. Configured webpack.config.js .

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import path from "path";
+const __dirname = path.resolve();
+
+export default {
+  mode: "development",
+  entry: "./src/main.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    },
+  },
+};
